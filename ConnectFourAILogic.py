fd30d6e8c699386b82e34aeee21cc52acdddee0b
MohamedMaGdY888 <134002283+MohamedMaGdY888@users.noreply.github.com>
2023-05-18 23:53:26 +0300

    Add files via upload

---
+++ ConnectFourAILogic.py
@@ -0,0 +1,47 @@
+from ConnectFourFunctions import *
+import random
+
+
+def minmax(board, depth, alpha, beta, maximizing_player):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, 2):
+                return (None, 100000000000000)
+            elif winning_move(board, 1):
+                return (None, -100000000000000)
+            else: # Game is over, no more valid moves
+                return (None, 0)
+        else: # Depth is zero
+            return (None, score_position(board, 2))
+    if maximizing_player:
+        value = float('-inf')
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            temp_board = board.copy()
+            drop_piece(temp_board, row, col, 2)
+            new_score = minmax(temp_board, depth-1, alpha, beta, False)[1]
+            if new_score > value:
+                value = new_score
+                column = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return column, value
+    else: # Minimizing player
+        value = float('inf')
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_next_open_row(board, col)
+            temp_board = board.copy()
+            drop_piece(temp_board, row, col, 1)
+            new_score = minmax(temp_board, depth-1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return column, value
